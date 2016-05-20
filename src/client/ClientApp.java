43dd4d6069f68a8f3bc300eee083d6abcd4d49ef
Sunnari <jsunnari@gmail.com>
2016-05-20 10:13:02 +0200

    Arrange packages and classes

--- src/client/ClientApp.java
+++ src/client/ClientApp.java
@@ -1,9 +1,25 @@
 package client;
 
+import client.gui.MainView;
+import client.logic.ClientController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by Jonas on 2016-05-19.
  */
 
-public class ClientApp {
+public class ClientApp extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MainView mainView = new MainView(primaryStage);
+        ClientController clientController = new ClientController(mainView);
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
 
 }
---
+++ src/client/logic/ClientController.java
@@ -0,0 +1,17 @@
+package client.logic;
+
+import client.gui.MainView;
+
+/**
+ * Created by Jonas on 2016-05-20.
+ */
+
+public class ClientController{
+    MainView view;
+
+    public ClientController(MainView view) {
+        this.view = view;
+
+    }
+
+}
